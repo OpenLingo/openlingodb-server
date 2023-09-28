a5689daa2f8669feda266aa8cfb6aef63a690e1b
Jessie Harvey <jessie.harvey120@gmail.com>
2023-09-28 18:07:05 +1000

    altered language view to send dialects in response

--- views/language.py
+++ views/language.py
@@ -1,6 +1,7 @@
 from app import app, ROUTE_PREFIX
 from flask import jsonify
 from services import language as service
+from services import dialect as dialect_service
 
 
 @app.route(ROUTE_PREFIX + "language", methods=['GET'])
@@ -13,6 +14,8 @@ def get_languages():
     http://127.0.0.1:5000/api/language
     """
     languages = service.get_all_languages()
+    for language in languages:
+        language['dialects'] = dialect_service.get_dialects_by_language(language['id'])
 
     if not languages:
         return "404 not found", 404
@@ -33,6 +36,7 @@ def get_language(language_id: int):
     where 'id' is the id of the requested language.
     """
     language = service.get_language(language_id)
+    language['dialects'] = dialect_service.get_dialects_by_language(language['id'])
 
     if not language:
         return "langauge not found", 404
